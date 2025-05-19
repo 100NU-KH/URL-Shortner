3ebf7c40b474730e780369bc00082bda6afe4570
Sonu Khandwal <sonukhandwal@Sonus-MacBook-Pro.local>
2025-05-19 23:25:56 +0530

    error message updated

--- URLShortner/main.py
+++ URLShortner/main.py
@@ -1,5 +1,5 @@
-import yaml
-from flask import Flask, Blueprint, redirect
+import yaml, json
+from flask import Flask, Blueprint, redirect, Response
 from database import get_db
 from apps.shortner.routes.create import shortner_app_route
 from apps.shortner import models
@@ -16,6 +16,12 @@ root_url = Blueprint('root', __name__)
 def dashboard(hash_str):
     db = get_db()
     tinyurl_obj = db.query(models.URLMapper).filter(models.URLMapper.hash_str==hash_str).first()
+    if not tinyurl_obj:
+        return Response(json.dumps({
+            "message": "Invalid input",
+            "success": False
+        }), status=400,
+        mimetype="application/json")
     return redirect(tinyurl_obj.url,code=302)
 #############################
 
